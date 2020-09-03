165da8118cc5d34b809faf6eac58e56ab08134fc
Ellen <ellen.white@fdmgroup.com>
2020-09-03 13:14:19 -0500

    router module implemented

--- my-first-app/src/app/app-routing.module.ts
+++ my-first-app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
 ];
 
+//The method is called forRoot() because you configure the router at the application's root level. The forRoot() method supplies the service providers and directives needed for routing, and performs the initial navigation based on the current browser URL.
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
