2aa36d5f9aeefe37fd949faccdca95aeaad03ad9
Ellen <ellen.white@fdmgroup.com>
2020-09-03 16:34:36 -0500

    in-memory-api set up as mock db, additional styling

--- my-first-app/src/app/hero.service.ts
+++ my-first-app/src/app/hero.service.ts
@@ -7,6 +7,7 @@ import { MessageService } from './message.service';
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -14,17 +15,27 @@ import { HEROES } from "./mock-heroes";
 
 export class HeroService {
 
+  private heroesURL = 'api/heroes';
+
   // . The assignment occurs synchronously, as if the server could return heroes instantly or the browser could freeze the UI while it waited for the server's response.
-  constructor(private _messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private _messageService: MessageService
+    ) { }
 
   getHeroes(): Observable<Hero[]>{
-    this._messageService.add("HeroService fetching heros");
-    return of(HEROES)
+    this.log("HeroService fetching heros");
+    return this.http.get<Hero[]>(this.heroesURL);
+    // return of(HEROES)
   }
   //Without this observable, a live application would crash since it is initalizing external data asynchronisly.. the injection of the Observable into the root along with the additon of the 'subscribe' method to heroesComponent.getHeros() allows the assignment occurs synchronously, as if the server could return heroes instantly or the browser could freeze the UI while it waited for the server's response.
   getHero(id: number): Observable<Hero> {
-    this._messageService.add(`HeroService: fetched hero with ID: ${id}`);
+    this.log(`HeroService: fetched hero with ID: ${id}`);
     return of(HEROES.find(hero=> hero.id === id));
+  }
+
+  private log(message: string) {
+    this._messageService.add(`HeroService: ${message}`);
   }
 
 }
