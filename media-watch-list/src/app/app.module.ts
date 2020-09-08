ae0cbcf1fcec14750e6094a0c4656efb3a964383
Ellen <ellen.white@fdmgroup.com>
2020-09-08 14:05:13 -0500

    initial commit for new Media Watch List app

---
+++ media-watch-list/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MediaItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
