1c9e3328a3437c879bbad1e90f1f97a1f6674c80
Ellen <ellen.white@fdmgroup.com>
2020-09-09 12:45:19 -0500

    form validation and messages added

--- media-watch-list/src/app/app.module.ts
+++ media-watch-list/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +23,8 @@ import { MediaItemFormComponent } from './media-item-form/media-item-form.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
